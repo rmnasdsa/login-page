3bf43953dd57d4a1630dd8658f6db88cfbfa6188
Mobina.Ghasempour <romi.ghasem@gmail.com>
2024-03-06 19:48:29 +0330

    operationLogin file created.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import loginForm from "./components/loginForm.js";
+import operationLogin from "./components/operationLogin.js";
 export const root = document.querySelector('.root');
 
 loginForm();
+operationLogin();
---
+++ src/components/operationLogin.js
@@ -0,0 +1,3 @@
+
+const operationLogin = () => { };
+export default operationLogin;
